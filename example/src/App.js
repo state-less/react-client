5ad75a00593bf490a5367dd7692281b72b6f66c4
Moritz Roessler <moritz.roessler@gmail.com>
2021-01-05 19:55:32 +0100

    chore: add main exports

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 
-import { ExampleComponent } from '@state-less/react-client'
+import { ErrorBoundary, ServerComponent, Provider, useProps, useAction} from '@state-less/react-client'
 import '@state-less/react-client/dist/index.css'
 
+const ServerTime = (props) => {
+  const {time} = useProps();
+  const date = new Date;
+  date.setTime(time);
+  return <div>
+    Hey {date.toLocaleTimeString()}
+  </div>
+}
+
+
+const Poll = (props) => {
+  const serverProps = useProps();
+  const {values = [], votes = []} = serverProps;
+  const vote = useAction('vote', 'onClick');
+
+  return <div>
+    <ul>
+      {values.map((value, i) => {
+        return <li>
+          {value} - {votes[i]} <button onClick={() => vote(i)}>Vote</button>
+        </li>        
+      })}
+    </ul>
+    <ruby>
+    <rb>WWW</rb><rt>World Wide Web</rt>
+  </ruby>
+  </div>
+}
+
+const Example = () => {
+  return <>
+      <ErrorBoundary>
+    <ServerComponent name="poll">
+        <Poll />
+    </ServerComponent>
+      </ErrorBoundary>
+    {/* <ServerComponent name="serverTime">
+      <ServerTime />
+    </ServerComponent> */}
+  </>
+}
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <Provider url="http://localhost:3000">
+    <Suspense fallback="Loading">
+      <Example />
+    </Suspense>
+  </Provider>
 }
 
 export default App
